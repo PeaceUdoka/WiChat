e2ff034b0062e108f6e1cf356f60408a2c8bc58b
Peace Udoka Azubuogu <azubuogupeace1@gmail.com>
2025-02-18 12:26:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def main():
                    st.markdown(response)
                    speak_text(response)
         with input_col3:
-            with sp.expander("📎")
+            with sp.expander("📎"):
                 if st.file_uploader(label = "", help=None):
                     pass
 
